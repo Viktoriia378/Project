b4fa380b885d77fbc04e9412b17bfc3204637790
Viktoriia378 <epifanova.victoria.arkadievna@gmail.com>
2025-05-19 22:05:33 +0200

    update

--- src/darksouls_game/DarkSoulsWindow.java
+++ src/darksouls_game/DarkSoulsWindow.java
@@ -1,4 +1,5 @@
 package darksouls_game;
+
 import javax.swing.*;
 import java.awt.*;
 
@@ -7,8 +8,15 @@ public class DarkSoulsWindow extends JFrame {
     private JTextArea logArea;
     private JButton attackButton, estusButton, dodgeButton;
     private JLabel playerHpLabel, bossHpLabel;
-
     private final BattleManager battle;
+    private final CardLayout cardLayout = new CardLayout();
+    private final JPanel mainPanel = new JPanel();
+    // intro
+    private final JPanel introPanel = new JPanel(new BorderLayout());
+    private final JLabel imageLabel = new JLabel();
+    private final JLabel subtitleLabel = new JLabel("", SwingConstants.CENTER);
+    //fight
+    private final JPanel battlePanel = new JPanel(new BorderLayout());
 
     public DarkSoulsWindow() {
         Player player = new Player();
@@ -16,12 +24,11 @@ public class DarkSoulsWindow extends JFrame {
         battle = new BattleManager(player, boss);
 
         setTitle("Dark Souls Mini");
-        setSize(600, 400);
-        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        setLayout(new BorderLayout());
+        setSize(400, 400); //sets the x-dimension and y-dimension
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE); // exit out of application
+        ImageIcon icon = new ImageIcon("img/256x256.png");
+        setIconImage(icon.getImage());
 
-        logArea = new JTextArea();
-        logArea.setEditable(false);
 
         // panel HP
         JPanel statusPanel = new JPanel(new GridLayout(1, 2));
@@ -29,16 +36,16 @@ public class DarkSoulsWindow extends JFrame {
         bossHpLabel = new JLabel();
         statusPanel.add(playerHpLabel);
         statusPanel.add(bossHpLabel);
-        add(statusPanel, BorderLayout.NORTH);
+
+        battlePanel.add(statusPanel, BorderLayout.NORTH);
+
 
         // log
-
         logArea = new JTextArea();
         logArea.setEditable(false);
-        add(new JScrollPane(logArea), BorderLayout.CENTER);
+        battlePanel.add(new JScrollPane(logArea), BorderLayout.CENTER);
 
-// Panel with buttons
-
+        // Panel with buttons
         attackButton = new JButton("Attack");
         estusButton = new JButton("Drink estus");
         dodgeButton = new JButton("Dodge");
@@ -51,29 +58,68 @@ public class DarkSoulsWindow extends JFrame {
         buttonPanel.add(attackButton);
         buttonPanel.add(estusButton);
         buttonPanel.add(dodgeButton);
-        add(buttonPanel, BorderLayout.SOUTH);
+        battlePanel.add(buttonPanel, BorderLayout.SOUTH);
 
         updateStatus();
-        log("You woke up by the bonefire... Forward into battle!");
+        log("You woke up by the bonfire... Forward into battle!");
+
+        //intro panel
+
+        imageLabel.setHorizontalAlignment(SwingConstants.CENTER);
+        subtitleLabel.setPreferredSize(new Dimension(400, 100));
+        subtitleLabel.setFont(new Font("Serif", Font.PLAIN, 18));
+
+        introPanel.add(imageLabel, BorderLayout.CENTER);
+        introPanel.add(subtitleLabel, BorderLayout.SOUTH);
+
         setVisible(true);
+        mainPanel.add(introPanel, "intro");
+        mainPanel.add(battlePanel, "battle");
+        add(mainPanel);
+        startIntroSequence();
     }
-    public void handleAction(String action){
-        if(battle.isGameOver()) return;
+
+    private void setImage(String path) {
+        ImageIcon icon = new ImageIcon(path);
+        Image scaled = icon.getImage().getScaledInstance(400, 400, Image.SCALE_SMOOTH);
+        imageLabel.setIcon(new ImageIcon(scaled));
+    }
+
+    private void startIntroSequence() {
+        cardLayout.show(mainPanel, "intro");
+        setImage("img/bonfire.png");
+        subtitleLabel.setText("You woke up by the bonfire...");
+        new javax.swing.Timer(3000, e1 -> {
+            setImage("img/dark.png");
+            subtitleLabel.setText("You are going to the dark...");
+
+            new javax.swing.Timer(3000, e2 -> {
+                setImage("img/fight.png");
+                subtitleLabel.setText("for the battle!");
+                new javax.swing.Timer(3000, e3 -> {
+                    startBattleUI();
+                }).start();
+            }).start();
+        }).start();
+    }
+
+    public void handleAction(String action) {
+        if (battle.isGameOver()) return;
 
         switch (action) {
-            case "attack" -> log (battle.playerAttack());
-            case "heal" -> log (battle.playerHeal());
-            case "dodge" -> log (battle.playerDodge());
+            case "attack" -> log(battle.playerAttack());
+            case "heal" -> log(battle.playerHeal());
+            case "dodge" -> log(battle.playerDodge());
         }
 
-        if(!battle.isGameOver()){
+        if (!battle.isGameOver()) {
             log(battle.bossTurn());
         }
 
         updateStatus();
 
-        if(battle.isGameOver()){
-            if(battle.playerWon()){
+        if (battle.isGameOver()) {
+            if (battle.playerWon()) {
                 log("VICTORY" + "\nSouls received: 500");
             } else {
                 log("YOU DEAD");
@@ -81,14 +127,21 @@ public class DarkSoulsWindow extends JFrame {
             disableButtons();
         }
     }
-    private void updateStatus(){
+    private void startBattleUI(){
+        cardLayout.show(mainPanel, "battle");
+        updateStatus();
+        log("He is coming...");
+    }
+    private void updateStatus() {
         playerHpLabel.setText("Knight HP: " + Math.max(battle.getPlayerHp(), 0));
         bossHpLabel.setText("Boss HP: " + Math.max(battle.getBossHp(), 0));
     }
-    public void log(String text){
+
+    public void log(String text) {
         logArea.append(text + "\n");
     }
-    private void disableButtons(){
+
+    private void disableButtons() {
         attackButton.setEnabled(false);
         estusButton.setEnabled(false);
         dodgeButton.setEnabled(false);
